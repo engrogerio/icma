528d08c9c0b4fd0e8eeefef43bae2037f28f2926
Rogerio Silva <eng.rogerio@gmail.com>
2021-04-07 20:04:23 -0300

    adinfo serializer

--- excel.py
+++ excel.py
@@ -1,3 +1,5 @@
+import os
+import json
 from openpyxl import Workbook
 from openpyxl import load_workbook
 
@@ -8,20 +10,52 @@ class AdInfo:
         :param: adinfo_template: filename for adinfo json template.
 
         '''
-        self.adinfo_template = adinfo_template
+        self.adinfo_template = adinfo_template or 'adinfo.json'
 
-    def open_file(self, filename):
+    def _open_file(self, filename):
+        print(filename)
         wb = load_workbook(filename=filename)
         return wb
 
-    def get_adinfo_data(self, adinfo_filename, adinfo_template=self.adinfo_template):
-        data = self.open_file(filename).active
-        json_template = None
-        json_adinfo_heading = None
-        with open(adinfo_template, 'r') as template:
-            json_template = json.load(template)
-        for key in json_template['heading']:
-            json_adinfo_heading[key] = data[json_template[key]]
+    def adinfo_serializer(self, adinfo_filename):
+        """
+        Get adinfo filename and template and creates a dictionary with
+        all the adinfo data.
+        This will not work for formulas cells.
 
+        :adinfo_filename: The excel adinfo filename.
 
- 
+        return a dictionary version of the adinfo file.
+        """
+        """
+        If formula cells are needed, should use:
+        import xlwings as xw
+        wbxl=xw.Book('to_erase.xlsx')
+        wbxl.sheets['Sheet'].range('B3').value
+
+        """
+        adinfo_data = self._open_file(adinfo_filename).active
+        template_dict = dict()
+        adinfo_dict = dict(heading={}, lines={})
+        
+        with open(self.adinfo_template, 'r') as template:
+            template_dict = json.load(template)
+        
+        for key, value in template_dict['heading'].items():
+            adinfo_dict['heading'][key] = adinfo_data[value].value
+
+        initial_row = (template_dict['lines']).pop('linha_inicial') + 1
+        final_row = adinfo_data.max_row
+
+        for row in range(initial_row, final_row):
+            adinfo_dict['lines'][row] = dict()
+            for value_name, column in template_dict['lines'].items():
+                adinfo_dict['lines'][row][value_name] = adinfo_data[
+                    column+str(row)].value
+        return adinfo_dict
+"""
+path = os.path.dirname(os.path.realpath(__file__))
+adinfo = AdInfo(path+'/adinfo/adinfo.json')
+adinfo_dict = adinfo.adinfo_serializer(path+'/adinfo/adinfo2.xlsx')
+print (adinfo_dict)
+"""
